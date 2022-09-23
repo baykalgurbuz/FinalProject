816877ce1f08dab014fd1b55df5223b7821093ec
enes baykal gurbuz <ebaykalgurbuz@gmail.com>
2022-09-24 01:48:20 +0200

    Add Autofac extension.

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concreate;
+using DataAccess.Abstract;
+using DataAccess.Concreate.EntitiyFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance(); 
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+        }
+    }
+}
